9cd695151628a0bcd6d9a67b59de415000d03dfa
vermadivi18 <vermadivyanshi64@gmail.com>
2026-02-03 16:22:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const socketIo = require('socket.io');
 const path = require('path');
 const fs = require('fs');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
 
 const app = express();
 const server = http.createServer(app);
